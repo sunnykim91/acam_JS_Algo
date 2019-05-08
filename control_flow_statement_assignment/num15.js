bf2d3d2a55a5355623ed99666b104a52a0046863
sunnykim91 <tjsgnl2002@gmail.com>
2019-05-08 11:21:04 +0900

    0508 add the assignment
    i add the assignment num14~num16.

--- control_flow_statement_assignment/num15.js
+++ control_flow_statement_assignment/num15.js
@@ -7,3 +7,18 @@
 //  *******
 // *********
 
+
+var line = 5;
+
+for (var i = 1; i < line*2; i+=2) {
+  for (var j = 1; j < ((line * 2) - i) / 2 ; j++) {
+    process.stdout.write(" ");
+  }
+  for( var l=1; l<=i;l++){
+    process.stdout.write("*");
+  }
+  for (var k = 1; k < ((line * 2) - i) / 2; k++) {
+    process.stdout.write(" ");
+  }
+  process.stdout.write("\n");
+}
--- control_flow_statement_assignment/num16.js
+++ control_flow_statement_assignment/num16.js
@@ -6,3 +6,19 @@
 //   *****
 //    ***
 //     *
+
+
+var line = 5;
+
+for (var i = (line*2) - 1 ; i >= 1; i -= 2) {
+  for (var j = 1; j < ((line * 2) - i) / 2; j++) {
+    process.stdout.write(" ");
+  }
+  for (var l = i; l >= 1; l--) {
+    process.stdout.write("*");
+  }
+  for (var k = 1; k < ((line * 2) - i) / 2; k++) {
+    process.stdout.write(" ");
+  }
+  process.stdout.write("\n");
+}
